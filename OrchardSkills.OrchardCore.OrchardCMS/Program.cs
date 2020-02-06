31e27e25975411c8ec61421f94153ef9d819150d
Orchard Skills <Sales@OrchardSkills.com>
2020-02-06 13:40:12 -0700

    Added projects

---
+++ OrchardSkills.OrchardCore.OrchardCMS/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrchardCore.Logging;
+
+namespace OrchardSkills.OrchardCore.OrchardCMS
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => BuildHost(args).RunAsync();
+
+        public static IHost BuildHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseStartup<Startup>()
+                    .UseNLogWeb()
+                ).Build();
+    }
+}
